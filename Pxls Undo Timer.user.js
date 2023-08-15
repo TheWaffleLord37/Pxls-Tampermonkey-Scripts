574248a1e804e83172ad99996f573349da1add04
TheWaffleLord37 <102199627+TheWaffleLord37@users.noreply.github.com>
2023-08-15 06:55:43 -0700

    Update Pxls Undo Timer.user.js

--- Pxls Undo Timer.user.js
+++ Pxls Undo Timer.user.js
@@ -5,7 +5,7 @@
 // @description  Adds a 60 second timer under your stored pixels that appears whenever you undo a pixel. (Warning: This timer appears even if you still have 1 or 2 undos left before you have to wait 60 seconds for the next 3.) (Another warning: this is not 100% accurate, it might be off by a few seconds.)
 // @author       Vanilla
 // @match        https://pxls.space/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=pxls.space
+// @icon         https://pxls.space/favicon.ico
 // @updateURL    https://gist.githubusercontent.com/FlyingSixtySix/61d6f1034d1b192a4814b7e935d13d83/raw/pxls-undo-timer.userscript.js
 // @downloadURL  https://gist.githubusercontent.com/FlyingSixtySix/61d6f1034d1b192a4814b7e935d13d83/raw/pxls-undo-timer.userscript.js
 // @grant        none
